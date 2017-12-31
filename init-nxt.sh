4e2ac6e3aa440f822dd2bdd9fb63fe339c423158
ronan <ryl@free.fr>
2017-12-31 13:15:03 +0100

    Some cleanup

--- init-nxt.sh
+++ init-nxt.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# if the admin password is defined in the ENV variable, we append to the config
+if [ -n "${ADMINPASSWD-}" ]; then
+	echo "ADMINPASSWD provided"
+else
+	echo "ADMINPASSWD not provided"
+fi
+
+# We figure out what is the current db folder
+if [ "${NXTNET:=test}" = "main" ]; then
+	TESTNET=false
+else
+	TESTNET=true
+fi
+
 if [ ! -f "/ardor/conf/version" ]; then
 	echo -e "init-nxt.sh: Performing version $NRSVersion init..."
 
@@ -21,12 +35,10 @@ if [ ! -f "/ardor/conf/version" ]; then
 		echo "PLUGINS not provided"
 	fi
 
-	# if we need to bootstrap, we do that first.
-	# Warning, bootstrapping will delete the current blockchain.
 	# $BLOCKCHAINDL must point to a zip that contains the nxt_db folder itself.
-	if [ -n "${BLOCKCHAINDL-}" ] && [ ! -d "$DB" ]; then
-		echo "init-nxt.sh: $DB not found, downloading blockchain from $BLOCKCHAINDL";
-		wget "$BLOCKCHAINDL" && unzip *.zip && rm *.zip
+	if [ -n "${BLOCKCHAINDL-}" ]; then
+		echo "init-nxt.sh: downloading blockchain from $BLOCKCHAINDL";
+		wget "$BLOCKCHAINDL" && unzip *.zip && mv nxt_db nxt_${NXTNET}_db && rm *.zip
 		echo "init-nxt.sh: Blockchain download complete"
 	else
 		echo "BLOCKCHAINDL not provided"
@@ -35,32 +47,12 @@ if [ ! -f "/ardor/conf/version" ]; then
 	# If we did all of that, we dump a file that will signal next time that we
 	# should not run the init-script again
 	echo ${NRSVersion} >/ardor/conf/version
-else
-	echo -e "init-nxt.sh: Init already done, skipping init."
 fi
-
-# if the admin password is defined in the ENV variable, we append to the config
-if [ -n "${ADMINPASSWD-}" ]; then
-	echo "ADMINPASSWD provided"
-else
-	echo "ADMINPASSWD not provided"
-fi
-
-# We figure out what is the current db folder
-if [ "${NXTNET:=test}" = "main" ]; then
-	DB="nxt_db"
-	TESTNET=false
-else
-	DB="nxt_test_db"
-	TESTNET=true
-fi
-# just to be sure :)
-echo "Database is $DB"
 
 echo "init-nxt.sh: Preparing config for ${NXTNET} net"
 sed -e "s/ADMINPASSWD/${ADMINPASSWD-}/g" \
 	-e "s/TESTNET/${TESTNET}/g" \
-	-e "s/NXTNET/${NXTNET:-test}/g" \
+	-e "s/NXTNET/${NXTNET}/g" \
 	-e "s/MYPLATFORM/${MYPLATFORM:-Docker}/g" \
 	-e "s/MYADDRESS/${MYADDRESS}/g" \
 	-e 's/\r$//g' \
